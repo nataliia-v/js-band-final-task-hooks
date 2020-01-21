0258143085e5d1891bf9c837fa5c0e3f1c8cc6ad
nataliia-v <nataly.verbenskaya@gmail.com>
2020-01-21 15:05:04 +0200

    * edited Cart component to functional component

--- src/pages/PrivateRoutes/Cart/Cart.js
+++ src/pages/PrivateRoutes/Cart/Cart.js
@@ -1,41 +1,38 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import React from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 
-import Button from 'components/Button/Button';
-import Table from 'components/Table/Table';
-import { cartPurchase } from 'store/cart/thunks';
-import { getCartBooks } from 'store/cart/selectors';
+import Button from "components/Button/Button";
+import Table from "components/Table/Table";
+import { cartPurchase } from "store/cart/thunks";
+import { getCartBooks } from "store/cart/selectors";
 
-import EmptyState from './EmptyState/EmptyState';
-import cartTableColumns from './cartTableColumns';
+import EmptyState from "./EmptyState/EmptyState";
+import cartTableColumns from "./cartTableColumns";
 
-import styles from './Cart.module.scss';
+import styles from "./Cart.module.scss";
 
-class Cart extends Component {
-  onPurchase = () => {
-    const { actions } = this.props;
+
+function Cart({ cartBooks, actions }) {
+
+  const onPurchase = () => {
     actions.cartPurchase();
   };
 
-  render() {
-    const { cartBooks } = this.props;
-
-    return (
-      <div className={styles.wrapper}>
-        <div className={styles.btnWrapper}>
-          <Button disabled={!cartBooks.length} onClick={this.onPurchase}>
-            Purchase
-          </Button>
-        </div>
-        {cartBooks.length ? (
-          <Table columns={cartTableColumns} data={cartBooks} />
-        ) : (
-          <EmptyState />
-        )}
+  return (
+    <div className={ styles.wrapper }>
+      <div className={ styles.btnWrapper }>
+        <Button disabled={ !cartBooks.length } onClick={ onPurchase }>
+          Purchase
+        </Button>
       </div>
-    );
-  }
+      { cartBooks.length ? (
+        <Table columns={ cartTableColumns } data={ cartBooks }/>
+      ) : (
+        <EmptyState/>
+      ) }
+    </div>
+  );
 }
 
 const mapStateToProps = state => ({
